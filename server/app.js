c4d236d4012bedb025669408f58aab1fc36e718b
Claudia Calderas <claudia.calderas@gmail.com>
2017-03-24 14:40:36 -0500

    router to get information of owners and their pets

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 var port = 5000;
 var bodyParser = require('body-parser');
+var display = require('./routes/display.js');
 
 
 app.use(express.static("server/public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/display',display);
 
 
 
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   console.log("jQuery sourced");
+
+  // displays information from the database on the DOM
+  displayInformation();
+
 });
+
+function displayInformation() {
+  $.ajax({
+    type: "GET",
+    url: "/display",
+    success: function(response) {
+      console.log("displayInformation: ",response);
+    }
+  });
+}
