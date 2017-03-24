d4185ebbfaae093ec07b6c823168cf482309b3fd
Teigen Leonard <teigenl@gmail.com>
2017-03-24 15:01:00 -0500

    add event listener on pet submit and client pet POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
   console.log("jQuery sourced");
+
+
+  $( '#add_pet_button' ).on( 'submit', function(){
+      e.preventDefault();
+      console.log( 'in add_pet_button on submit');
+      var pet = {};
+      pet.name = $( '#pet_name').val();
+      pet.color = $( '#pet_color').val();
+      pet.breed = $( '#pet_breed').val();
+
+      $.ajax({
+       type: 'POST',
+       url: '/newPet',
+       data: pet,
+       success: function(response) {
+         console.log("in client POST function");
+         //claudias function;
+       }
+     });
+
 });
