bc72e03b600d2ccdeaa5acc86bb4c415dbda7cde
Erin Kinnen <erin.kinnen@outlook.com>
2017-03-24 11:57:55 -0500

    Initial commit.

---
+++ server/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var port = 5000;
+var bodyParser = require('body-parser');
+
+
+app.use(express.static("server/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+
+
+app.listen(port, function(){
+  console.log("Listening on port: ", port);
+}
+);
