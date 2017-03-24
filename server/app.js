c6c42e493becd700221bb4da8efdd8625effda7d
Teigen Leonard <teigenl@gmail.com>
2017-03-24 14:41:10 -0500

    add newPet post to app.js

--- server/app.js
+++ server/app.js
@@ -3,13 +3,20 @@ var app = express();
 var port = 5000;
 var bodyParser = require('body-parser');
 
+var pets = [];
+
 
 app.use(express.static("server/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 
-
+app.post( '/newPet', function(req, res){
+  console.log('newPet base hit');
+  var newPet = req.body;
+  pets.push(newPet);
+  res.sendStatus(200);
+});
 
 
 
